3ee7fa801d547a260a4be99cbcab0c0c5d3553e9
diarleynewshop-max <diarley.newshop@gmail.com>
2026-03-02 13:58:44 -0300

    Initial commit

---
+++ trigger.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "@trigger.dev/sdk/v3";
+
+export default defineConfig({
+  project: "proj_cyowyemtgsdaobsofdmb",
+  runtime: "node",
+  dirs: ["./src/trigger"],  
+  maxDuration: 5,
+});
